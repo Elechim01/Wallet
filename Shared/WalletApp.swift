5f06ab6b121c9fecad2f6fe933470010703918c4
Elechim01 <mikymanny015@gmail.com>
2021-05-02 22:41:33 +0200

    Initial Commit

---
+++ Shared/WalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 02/05/21.
+//
+
+import SwiftUI
+
+@main
+struct WalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
